58ef8188f2d33de9b44753d20e15549194394028
novovicn <novovicnenad96@gmail.com>
2022-03-07 21:51:26 +0100

    added goal model, get and post routes

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,18 +1,33 @@
 const asyncHandler = require('express-async-handler');
+const Goal = require('../models/goalModel');
 
-const getGoals = asyncHandler(async (req, res) =>
-  res.json({ msg: 'Get goals' })
-);
+const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find({});
+  res.status(200).json(goals);
+});
+
 const addGoal = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
+  const text = req.body.text;
+  if (!text) {
     res.status(400);
     throw new Error('Please enter goal!');
   }
+
+  try {
+    const goal = await Goal.create({text});
+    res.status(201).json(goal);
+  } catch (error) {
+    throw new Error('Invalid data!');
+  }
+  
+
   res.json({ msg: 'Add a goal' });
 });
+
 const updateGoal = asyncHandler(async (req, res) =>
   res.json({ msg: `Update goal ${req.params.id}` })
 );
+
 const deleteGoal = asyncHandler(async (req, res) =>
   res.json({ msg: `Delete goal ${req.params.id}` })
 );
