8b74cb6dfc6f04458e89ec47369a87b9fc64501f
novovicn <novovicnenad96@gmail.com>
2022-03-21 18:43:35 +0100

    added register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,75 @@
+import { useState } from 'react';
+import { FaUser } from 'react-icons/fa';
+
 function Register() {
-    return (
-        <div>
-            Register
-        </div>
-    )
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+  const dataChangeHandler = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const submitHandler = (e) => {
+      e.preventDefault();
+      console.log(name, email, password, password2);
+  }
+
+  return (
+    <>
+      <h1>
+        <FaUser /> Register
+      </h1>
+      <section className="form">
+        <form onSubmit={submitHandler}>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Enter your name"
+              onChange={dataChangeHandler}
+              name="name"
+              value={name}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Enter your e-mail"
+              onChange={dataChangeHandler}
+              name="email"
+              value={email}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Enter your password"
+              onChange={dataChangeHandler}
+              name="password"
+              value={password}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm your password"
+              onChange={dataChangeHandler}
+              name="password2"
+              value={password2}
+            />
+          </div>
+          <button type='submit' className='btn btn-block'>REGISTER</button>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Register
+export default Register;
