add8bda1cfce8ab80102e42c1f86bd093fa0f45c
novovicn <novovicnenad96@gmail.com>
2022-03-21 19:21:32 +0100

    added login page and concurrently setup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,55 @@
+import { useState } from 'react';
+import { FaUser } from 'react-icons/fa';
+
 function Login() {
-    return (
-        <div>
-            Login
-        </div>
-    )
+  const [formData, setFormData] = useState({
+    email: '',
+    password2: '',
+  });
+
+  const { email, password } = formData;
+  const dataChangeHandler = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const submitHandler = (e) => {
+      e.preventDefault();
+      console.log(email, password);
+  }
+
+  return (
+    <>
+      <h1>
+        <FaUser /> Login
+      </h1>
+      <section className="form">
+        <form onSubmit={submitHandler}>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Enter your e-mail"
+              onChange={dataChangeHandler}
+              name="email"
+              value={email}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Enter your password"
+              onChange={dataChangeHandler}
+              name="password"
+              value={password}
+            />
+          </div>
+          <button type='submit' className='btn btn-block'>REGISTER</button>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Login
+export default Login;
