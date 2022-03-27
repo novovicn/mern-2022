1b3440a1cf70c949bb7d4efe620a3d2356d53fde
novovicn <novovicnenad96@gmail.com>
2022-03-27 15:22:20 +0200

    added logout case to extrareducers

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -23,6 +23,11 @@ export const register = createAsyncThunk('auth/register', async (user, thunkAPI)
     }
 })
 
+// Logout user
+export const logout = createAsyncThunk('auth/logout', async () => {
+    await authService.logout();
+})
+
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
@@ -50,6 +55,9 @@ export const authSlice = createSlice({
                 state.error = true
                 state.message = action.payload
                 state.user = null
+            })
+            .addCase(logout.fulfilled, (state) => {
+                state.user = null;
             })
     }
 })
