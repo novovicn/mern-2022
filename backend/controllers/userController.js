6fb890b875578026ec49b673285f08d0fd4fa318
novovicn <novovicnenad96@gmail.com>
2022-03-27 14:11:22 +0200

    only user token gets passed to client

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,10 +24,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (user) {
     res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
+      token: generateToken(user._id)
     });
   } else {
     res.status(400);
@@ -42,10 +39,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
   if (user && (await bcrypt.compare(password, user.password))) {
     res.status(200).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
+      token: generateToken(user._id)
     });
   } else {
     res.status(400);
