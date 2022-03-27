ed731a1a5f09ca28f2e24658859ddec0ba662a93
novovicn <novovicnenad96@gmail.com>
2022-03-27 14:16:02 +0200

    bug saving user data in local storage fixed

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -7,7 +7,7 @@ const register = async (userData) => {
     const response = await axios.post(API_URL, userData);
 
     if(response.data){
-        localStorage.setItem('user', JSON.stringify(userData));
+        localStorage.setItem('user', JSON.stringify(response.data));
     }
 
     return response.data;
