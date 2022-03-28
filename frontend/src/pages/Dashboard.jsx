34998378630a8deda7b117926734f243b22d792e
novovicn <novovicnenad96@gmail.com>
2022-03-28 13:52:22 +0200

    added guard to dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { useSelector } from 'react-redux'; 
+import { useNavigate } from 'react-router-dom';
+
+
+
 function Dashboard() {
+
+    const { user } = useSelector(state => state.auth);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        !user && navigate('/login');
+    }, [user, navigate])
     return (
         <div>
             Dashboard
